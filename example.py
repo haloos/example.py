f3c17d42662dda60c4258f766f5c5419c62bb907
Jason Paredes <haloos@me.com>
2021-08-05 13:30:59 -0400

    Add files via upload
    Writing my first program in Python

--- example.py
+++ example.py
@@ -1,8 +1,2 @@
-n1 = input("Enter a noun:") 
-v = input("Enter a verb:") 
-adj = input("Enter a adj:")
-n2 = input("Enter a noun:") 
-
-r = """The {} {} the {} {} """.format(n1, v, adj, n2) 
-
-print(r) 
+for i in range(100): 
+  print("Hello, World!")
