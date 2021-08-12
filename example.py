9955a5f3653b94ab00fcf8c0b1112b8a561f2b8a
Jason Paredes <haloos@me.com>
2021-08-12 13:29:48 -0400

    Add files via upload

--- example.py
+++ example.py
@@ -1,10 +1,4 @@
-list1 = [8, 19, 148, 4]
-list2 = [9, 1, 33, 83]
-list3 = []
-
-for i in list1:
-    for j in list2:
-        mult = i * j
-        list3.append(i * j)
-
-print(list3)
+x = 10
+while x >= 0:
+    print(x)
+    x -= 1
